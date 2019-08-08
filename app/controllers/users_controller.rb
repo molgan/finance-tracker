1c02de0f2432d1a588c7fe4b199eb070080e30c1
molgan <olga.matsuga@gmail.com>
2019-08-08 02:36:09 +0000

    Add friends search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,20 @@ class UsersController < ApplicationController
   end
   
   def my_friends
-    
+    @friendships = current_user.friends
   end
   
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = 'You have entered on empty search string'
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = 'No users match this search criteria' if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
+
 end
